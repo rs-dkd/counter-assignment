3ff7aa997353b3ed97ae04916a66745c8cd33792
rs-dkd <rdavidsegovia@gmail.com>
2024-11-20 19:42:49 -0500

    Update Counter.test.js
    Fixed Counter.test.js to properly test the rendering of the Counter component, rendering of the counter message, that the initial count is 0, that clicking the + increments the count, and - decrements the count.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,42 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText("Counter")).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const count = screen.getByTestId("count");
+  expect(count).toHaveTextContent("0");
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const increment = screen. getByText("+");
+  const count = screen.getByTestId("count");
+  fireEvent.click(increment);
+  expect(count).toHaveTextContent("1");
+  fireEvent.click(increment);
+  expect(count).toHaveTextContent("2");
+  fireEvent.click(increment);
+  expect(count).toHaveTextContent("3");
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const decrement = screen. getByText("-");
+  const count = screen.getByTestId("count");
+  fireEvent.click(decrement);
+  expect(count).toHaveTextContent("-1");
+  fireEvent.click(decrement);
+  expect(count).toHaveTextContent("-2");
+  fireEvent.click(decrement);
+  expect(count).toHaveTextContent("-3");
 });
